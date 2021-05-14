8faa1f06857e2a854984c7d6cb0bcc4798642679
mirtleonard <leonardmirt26@gmail.com>
2021-05-14 18:19:33 +0300

    insert into database from form

--- paste/views.py
+++ paste/views.py
@@ -1,9 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse, HttpResponseRedirect
+from django.urls import reverse
+from django.http import Http404
 from paste.models import Paste
-
+from django.utils import timezone
 # Create your views here.
 
 def index(request):
     latestPasteList = Paste.objects.order_by('-date')
     context = {'latestPasteList' : latestPasteList,}
     return render(request, 'PasteBin/index.html', context)
+
+def SavePaste(request):
+    paste = Paste(title = request.POST.get('title'), text = request.POST.get('text'), date =  timezone.now());
+    paste.save()
+    return index(request)
